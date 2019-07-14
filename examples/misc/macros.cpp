9d234f89f6cef4f95b837ad8fbc8f2f75a75dbf8
RugessNome <vincent.chambrin@gmail.com>
2019-07-14 13:26:28 +0200

    add 'macros' example

---
+++ examples/misc/macros.cpp
@@ -0,0 +1,20 @@
+
+// See https://stackoverflow.com/questions/14041453/why-are-preprocessor-macros-evil-and-what-are-the-alternatives
+
+// Checkout the result of "clang++ -E macros.cpp" 
+// then "clang++ macros.cpp -o macros"
+
+#include <iostream>
+
+#define NINETEEN_EIGHTY_FOUR 1948
+
+#define max(a, b) ((a) > (b) ? (a) : (b))
+
+int main() 
+{
+  std::cout << NINETEEN_EIGHTY_FOUR << "\n";
+
+  int a = 3, b = 2;
+
+  std::cout << max(++a, b) << "\n";
+}
