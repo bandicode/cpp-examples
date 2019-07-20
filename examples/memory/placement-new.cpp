1882fd232284777cddbb9f0f1e2324841209a600
RugessNome <vincent.chambrin@gmail.com>
2019-07-20 13:37:28 +0200

    add "placement-new" example

---
+++ examples/memory/placement-new.cpp
@@ -0,0 +1,39 @@
+
+#include <iostream> // cout
+
+struct Object
+{
+  int n;
+
+  Object(int val)
+    : n(val)
+  {
+    std::cout << "Object constructed\n";
+  }
+
+  ~Object()
+  {
+    std::cout << "Object destroyed\n";
+  }
+};
+
+int main() 
+{
+  // allocated memory buffer
+  std::unique_ptr<char[]> buffer{ new char[sizeof(Object)] };
+
+  // construct Object in buffer using placement-new
+  new (buffer.get()) Object(42);
+
+  // get pointer to & use Object
+  Object* obj = reinterpret_cast<Object*>(buffer.get());
+  std::cout << obj->n << "\n";
+
+  // destroy Object
+  obj->~Object();
+
+  // reuse buffer
+  new (buffer.get()) Object(7);
+  std::cout << obj->n << "\n";
+  obj->~Object();
+}
