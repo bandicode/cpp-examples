26c3c619da413f31d24522701383b9a93a1da872
RugessNome <vincent.chambrin@gmail.com>
2019-07-20 14:08:02 +0200

    add "for-range" example

---
+++ examples/language/for-range.cpp
@@ -0,0 +1,56 @@
+
+#include <iostream>
+#include <list>
+#include <vector>
+
+int main() 
+{
+  // try changing vector to list
+  std::vector<int> digits = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+  // bad: only works for random access containers
+  for (size_t i(0); i < digits.size(); ++i)
+  {
+    std::cout << digits.at(i) << " ";
+  }
+
+  std::cout << std::endl;
+
+  // better: works for all containers
+  // cons: it is only used to read the container's elements
+  for (auto it = digits.begin(); it != digits.end(); ++it)
+  {
+    std::cout << *it << " ";
+  }
+
+  std::cout << std::endl;
+
+  // best: range-for loop
+  // pros: no useless iterator is visible
+  for (const int d : digits)
+  {
+    std::cout << d << " ";
+  }
+
+  std::cout << std::endl;
+
+  // good: iterators are used to remove elements from the container
+  for (auto it = digits.begin(); it != digits.end();)
+  {
+    if (*it % 2 == 0)
+    {
+      it = digits.erase(it);
+    }
+    else
+    {
+      ++it;
+    }
+  }
+
+  for (const int d : digits)
+  {
+    std::cout << d << " ";
+  }
+
+  std::cout << std::endl;
+}
