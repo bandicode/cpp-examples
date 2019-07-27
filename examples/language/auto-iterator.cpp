72761ba85eec0db3f10ece5724824501d870ebb4
RugessNome <vincent.chambrin@gmail.com>
2019-07-27 11:18:39 +0200

    fix typo in "auto-iterator" example

--- examples/language/auto-iterator.cpp
+++ examples/language/auto-iterator.cpp
@@ -8,7 +8,7 @@ int main()
   // try changing vector to list
   std::vector<int> digits = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
-  // bad: loop is only works with vector
+  // bad: loop will only works with vector
   for (std::vector<int>::const_iterator it = digits.begin(); it != digits.end(); ++it)
   {
     std::cout << *it << " ";
@@ -16,7 +16,7 @@ int main()
 
   std::cout << std::endl;
 
-  // good: loop does rely on actual type of digits
+  // good: loop does not rely on actual type of digits
   for (auto it = digits.begin(); it != digits.end(); ++it)
   {
     std::cout << *it << " ";
