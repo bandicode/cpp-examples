cbc59692e3ea0463907705ca766a2b482072fa6e
RugessNome <vincent.chambrin@gmail.com>
2019-09-01 12:23:57 +0200

    add "language/object-lifetime" example

---
+++ examples/language/object-lifetime.cpp
@@ -0,0 +1,42 @@
+
+#include <iostream>
+
+struct X
+{
+  int n;
+
+  X(int val)
+    : n(val)
+  {
+    std::cout << n << "\n";
+  }
+
+  ~X()
+  {
+    std::cout << "~" << n << "\n";
+  }
+};
+
+X f(X x)
+{
+  x.n += 1;
+  return x;
+}
+
+
+int main() 
+{
+  X x{ 1 };
+
+  std::cout << "Entering {}-scope" << "\n";
+
+  {
+    X y{ 2 };
+  }
+
+  std::cout << "Just leaved {}-scope" << "\n";
+
+  X z = X{ 3 }.n + X{ 4 }.n;
+
+  std::cout << "About to leave main()" << "\n";
+}
