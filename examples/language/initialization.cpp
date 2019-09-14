3dc18ccd13ed6c591ed90ae8a23813a6deb9ac85
RugessNome <vincent.chambrin@gmail.com>
2019-09-14 14:19:37 +0200

    update "language/initialization"

--- examples/language/initialization.cpp
+++ examples/language/initialization.cpp
@@ -12,6 +12,7 @@ struct X
 
 struct Y
 {
+  int y = 0;
   Y(X) { }
 };
 
@@ -21,7 +22,12 @@ struct Point
   int y;
 };
 
-enum class Color
+enum Device
+{
+  Keyboard, Mouse
+};
+
+enum class Color // enum-class values do not leak in parent scope
 {
   Red, Green, Blue
 };
@@ -30,43 +36,52 @@ int main()
 {
   /* Variable declaration without an initializer */
 
-  int n; // bad: value of 'n' is unpredictable (UB)
-  X x; // ok: x is default constructed
+  int integer_default_init; // bad: value is unpredictable (UB)
+  int* pointer_to_integer_default_init; // bad: value is unpredictable (UB)
+  X object_default_init; // ok: object is default constructed
 
   /* Value initialization */
 
-  int n2{}; // good: n2 is zero-initialized
-  // X x2(); // bad: declares a function !
-  X x3{};
+  int integer_value_init{}; // good: integer is zero-initialized
+  int* pointer_to_integer_value_init{}; // good: pointer is initialized with nullptr
+  // X object_value_init(); // bad: declares a function !
+  X object_value_init{};
 
   /* Initialization with an expression */
 
-  const int n3 = 7;
-  const auto x4 = X();
-  const auto x5 = X{};
+  const int integer_copy_init = 7;
+  const auto object_copy_init_arg = X();
+  const auto object_copy_init_brace = X{};
+  const int* pointer_to_integer_copy_init = &integer_copy_init;
 
   /* Direct () & List {} initialization */
 
-  int n4(3.14); 
-  int n5{ static_cast<int>(3.14) }; // list-initialization forbids narrowing conversion
-  X x6(1, 2);
-  X x7{ 1, 2 };
-  X x8(1);
-  X x9{ 1 };
-  // Y y1(X()); // bad: most vexing parse !
-  Y y2{ X() };
+  int integer_direct_init(3.14); 
+  int integer_brace_init{ static_cast<int>(3.14) }; // list-initialization forbids narrowing conversion
+  int* pointer_to_integer_arg_init(&integer_value_init);
+  int* pointer_to_integer_brace_init{ &integer_value_init };
+  X object_direct_init(1, 2);
+  X object_brace_init{ 1, 2 };
+  X object_direct_init_b(1);
+  X object_brace_init_b{ 1 };
+  // Y object_direct_init_c(X()); // bad: most vexing parse !
+  Y object_brace_init_c{ X() };
 
   /* Reference initialization */
 
-  int& n_ref_init_expr = n4;
-  int& n_ref_init_arg(n4);
-  int& n_ref_init_brace{ n4 };
+  int& integer_ref_init_expr = integer_default_init;
+  int& integer_ref_init_arg(integer_direct_init);
+  int& integer_ref_init_brace{ integer_direct_init };
 
   /* Initialization of enums */
 
-  const Color color_init_expr = Color::Blue;
-  const Color color_init_arg(Color::Green);
-  const Color color_init_brace{ Color::Red };
+  const Device enum_init_expr = Keyboard;
+  const Device enum_init_arg(Mouse);
+  const Device enum_init_brace{ Keyboard };
+
+  const Color enumclass_init_expr = Color::Blue;
+  const Color enumclass_init_arg(Color::Green);
+  const Color enumclass_init_brace{ Color::Red };
 
   /* Aggregate initialization */
 
